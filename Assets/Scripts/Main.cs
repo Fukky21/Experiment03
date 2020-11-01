27518845805d1d3ce28c1bdcd7fcfd29db46270c
Fukky21 <r.fukky21@gmail.com>
2020-11-01 15:08:07 +0900

    [Add] ビルド中にファイル出力できるようにした

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [Header("Settings")]
+    public string fileName;
+
+    public GameObject Sphere;
+
+    private int phase = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (phase == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Sphere.GetComponent<Renderer>().material.color = Color.red;
+                if (OutputResult.initialize(fileName) == -1)
+                {
+                    Application.Quit();
+                }
+                phase++;
+            }
+        }
+        else if (phase == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Sphere.GetComponent<Renderer>().material.color = Color.blue;
+                OutputResult.writeResult(fileName);
+                Application.Quit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/OutputResult.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class OutputResult
+{
+    public static int initialize(string fileName)
+    {
+        // Resultsフォルダが存在しないときは作成する
+        string folderPath = Application.dataPath + "/Results";
+        if (!Directory.Exists(folderPath))
+        {
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            di.Create();
+        }
+
+        string filePath = Application.dataPath + "/Results/" + fileName;
+        FileInfo fi = new FileInfo(filePath);
+        string header = "This is Header";
+
+        if (!fi.Exists)
+        {
+            using (StreamWriter writer = fi.CreateText())
+            {
+                writer.WriteLine(header);
+                writer.Close();
+            }
+            return 0;
+        }
+        else
+        {
+            // ファイルが既に存在するときは、-1を返す
+            Debug.Log("File already exists");
+            return -1;
+        }
+    }
+
+    public static void writeResult(string fileName)
+    {
+        string filePath = Application.dataPath + "/Results/" + fileName;
+        FileInfo fi = new FileInfo(filePath);
+        string dataRow = "End of file";
+
+        using (StreamWriter writer = fi.AppendText())
+        {
+            writer.WriteLine(dataRow);
+            writer.Close();
+        }
+    }
+}
