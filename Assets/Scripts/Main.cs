125876fa8d00d9a6677b0fb5b5e2bbcdf71f90e6
Fukky21 <r.fukky21@gmail.com>
2020-11-25 16:52:23 +0900

    [Add] 本実験に必要な機能などを追加

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.XR;
 using Valve.VR;
 
 public class Main : MonoBehaviour
@@ -15,17 +17,29 @@ public class Main : MonoBehaviour
     public SteamVR_Action_Boolean RespTrigger;
     public Text GuideText;
     public GameObject FixationPoint;
-    public GameObject MovePoint;
+    public GameObject MovingPoint;
+    public GameObject TopBelt;
+    public GameObject BottomBelt;
     public AudioClip sound;
     AudioSource audioSource;
+    private string experimentDataFilePath;
+    private static StreamWriter experimentDataFileWriter;
     private int phase = 0;
-    private bool isFreeMode = false;
+    private bool isPracticeMode = false;
+    private int step = 0;
+    private int currentTrial = 1;
+    private int shotCount = 0;
+    private double[] beltSpeedRatio;
+    private float[] beltSpeedToLeft;
+    private float[] beltSpeedToRight;
+    private bool[] topBeltMoveToRight;
     private float time = 0;
     private float localTime = 0;
+    private float stimulusPresentationStartTime = 0;
     private bool isMovingToRight = false;
     private float currentAngle = 0;
-    private string selectModeText = "Press Up => Start Recording in Free mode\n\nPress Down => Start Recording in Fix mode";
-    private string finishRecordingText = "Finish Recording!\n\nPress ESC => Quit App";
+    private string selectModeText = "Press Up: Start Practice\n\nPress Down: Start Experiment";
+    private string finishRecordingText = "Finish\n\nPress ESC: Quit App";
 
     void Start()
     {
@@ -42,55 +56,121 @@ public class Main : MonoBehaviour
             Application.Quit();
         }
 
-        if (phase == 0)
+        if (phase == 0) // キャリブレーション終了後
         {
-            // キャリブレーション終了後
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 ChangeGuideText(selectModeText, true);
                 phase++;
             }
         }
-        else if (phase == 1)
+        else if (phase == 1)　// モード選択中
         {
-            // モード選択中
             if (RespUp.GetState(HandType))
             {
-                // Free mode
-                ChangeGuideText(null, false);
-                MovePoint.SetActive(true);
+                // Start Practice
+                MovingPoint.SetActive(true);
                 PupilLabs.EyeTrackingDataManager.StartRecording();
                 HeadTrackingDataManager.StartRecording();
-                isFreeMode = true;
+                isPracticeMode = true;
+                ChangeGuideText(null, false);
                 phase++;
             }
 
             if (RespDown.GetState(HandType))
             {
-                // Fix mode
-                ChangeGuideText(null, false);
-                FixationPoint.SetActive(true);
+                // Start Experiment
+                InitializeExprimentDataFile();
+                SetupParameters();
+                experimentDataFileWriter = new StreamWriter(experimentDataFilePath, true);
                 PupilLabs.EyeTrackingDataManager.StartRecording();
                 HeadTrackingDataManager.StartRecording();
-                isFreeMode = false;
+                isPracticeMode = false;
+                ChangeGuideText(trialText(currentTrial), true);
                 phase++;
             }
         }
-        else if (phase == 2)
+        else if (phase == 2) // 記録中
         {
-            // 記録中
-            Do();
-            time += Time.deltaTime;
-
-            if (RespTrigger.GetState(HandType))
+            if (isPracticeMode)
             {
-                PupilLabs.EyeTrackingDataManager.StopRecording();
-                HeadTrackingDataManager.StopRecording();
-                MovePoint.SetActive(false);
-                FixationPoint.SetActive(false);
-                ChangeGuideText(finishRecordingText, true);
-                phase++;
+                DoPractice();
+                time += Time.deltaTime;
+                // トリガーを引いたら終了
+                if (RespTrigger.GetState(HandType))
+                {
+                    PupilLabs.EyeTrackingDataManager.StopRecording();
+                    HeadTrackingDataManager.StopRecording();
+                    MovingPoint.SetActive(false);
+                    FixationPoint.SetActive(false);
+                    ChangeGuideText(finishRecordingText, true);
+                    phase++;
+                }
             }
+            else
+            {
+                DoExperiment();
+                time += Time.deltaTime;
+                // 規定のトライアル数を終えたとき、stepが6になる
+                if (step == 6)
+                {
+                    PupilLabs.EyeTrackingDataManager.StopRecording();
+                    HeadTrackingDataManager.StopRecording();
+                    experimentDataFileWriter.Close();
+                    ChangeGuideText(finishRecordingText, true);
+                    phase++;
+                }
+            }
+        }
+    }
+
+    int ConvertBoolToInt(bool b)
+    {
+        if (b)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    float Tan(float x)
+    {
+        return (float)Math.Tan(x * (Math.PI / 180));
+    }
+
+    Type[] FisherYates<Type>(Type[] arr)
+    {
+        System.Random rnd = new System.Random();
+        int n = arr.Length;
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = (int)Math.Floor(rnd.NextDouble() * (i + 1));
+            Type tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+        return arr;
+    }
+
+    Dictionary<string, float> ConvertRatioToSpeed(double ratio)
+    {
+
+        double fast = Settings.beltBaseSpeed * Math.Sqrt(Math.Pow(2, Math.Abs(ratio)));
+        double slow = Settings.beltBaseSpeed / Math.Sqrt(Math.Pow(2, Math.Abs(ratio)));
+
+        if (ratio > 0)
+        {
+            return new Dictionary<string, float>() {
+                {"speedToLeft", (float)Math.Round(slow, 1)},
+                {"speedToRight", (float)Math.Round(fast, 1)}
+            };
+        }
+        else
+        {
+            return new Dictionary<string, float>() {
+                {"speedToLeft", (float)Math.Round(fast, 1)},
+                {"speedToRight", (float)Math.Round(slow, 1)}
+            };
         }
     }
 
@@ -100,46 +180,196 @@ public class Main : MonoBehaviour
         MainCamera.GetComponent<Camera>().backgroundColor = Color.black;
     }
 
+    // Setup beltSpeedRatio, beltSpeedToLeft, beltSpeedToRight, topBeltMoveToRight
+    private void SetupParameters()
+    {
+        System.Random rnd = new System.Random();
+        double[] tmpSpeedRatio = new double[Settings.nTrials()];
+        beltSpeedRatio = new double[Settings.nTrials()];
+        beltSpeedToLeft = new float[Settings.nTrials()];
+        beltSpeedToRight = new float[Settings.nTrials()];
+        topBeltMoveToRight = new bool[Settings.nTrials()];
+
+        for (int i = 0; i < Settings.repeat; i++)
+        {
+            for (int j = 0; j < Settings.ratio.Length; j++)
+            {
+                tmpSpeedRatio[Settings.ratio.Length * i + j] = Settings.ratio[j];
+            }
+        }
+        Array.Copy(tmpSpeedRatio, beltSpeedRatio, tmpSpeedRatio.Length);
+        beltSpeedRatio = FisherYates(beltSpeedRatio);
+        for (int i = 0; i < beltSpeedRatio.Length; i++)
+        {
+            beltSpeedToLeft[i] = ConvertRatioToSpeed(beltSpeedRatio[i])["speedToLeft"];
+            beltSpeedToRight[i] = ConvertRatioToSpeed(beltSpeedRatio[i])["speedToRight"];
+        }
+        for (int i = 0; i < Settings.nTrials(); i++)
+        {
+            if (rnd.NextDouble() < 0.5)
+            {
+                topBeltMoveToRight[i] = false;
+            }
+            else
+            {
+                topBeltMoveToRight[i] = true;
+            }
+        }
+    }
+
+    private void InitializeExprimentDataFile()
+    {
+        // Resultsフォルダが存在しないときは作成する
+        string folderPath = Application.dataPath + "/Results";
+        if (!Directory.Exists(folderPath))
+        {
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            di.Create();
+        }
+
+        experimentDataFilePath = Application.dataPath + $"/Results/{fileName}_ex_data.txt";
+        FileInfo fi = new FileInfo(experimentDataFilePath);
+        string header = "trial ratio top_belt_move_to_left resp_up presentation_start_time";
+        using (StreamWriter sw = fi.CreateText())
+        {
+            sw.WriteLine(header);
+            sw.Close();
+        }
+    }
+
+    private async void WriteExData(bool respUp)
+    {
+        string dataRow = "";
+        dataRow += $"{currentTrial} {beltSpeedRatio[currentTrial - 1]}";
+        dataRow += $" {ConvertBoolToInt(topBeltMoveToRight[currentTrial - 1])} {ConvertBoolToInt(respUp)}";
+        dataRow += $" {stimulusPresentationStartTime}";
+        await experimentDataFileWriter.WriteLineAsync(dataRow);
+    }
+
     private void ChangeGuideText(string text, bool isEnabled)
     {
         GuideText.text = text;
         GuideText.enabled = isEnabled;
     }
 
-    private void Do()
+    private string trialText(int currentTrial)
     {
-        if (isFreeMode)
+        return $"{currentTrial}/{Settings.nTrials()}";
+    }
+
+    private void DoPractice()
+    {
+        if (isMovingToRight)
         {
-            if (isMovingToRight)
-            {
-                MovePoint.transform.RotateAround(Settings.cameraPosition, Vector3.up, Settings.movingSpeed() * Time.deltaTime);
-                currentAngle += Settings.movingSpeed() * Time.deltaTime;
+            MovingPoint.transform.RotateAround(Settings.cameraPosition, Vector3.up, Settings.movingSpeed() * Time.deltaTime);
+            currentAngle += Settings.movingSpeed() * Time.deltaTime;
 
-                if (currentAngle > Settings.movingAngle)
-                {
-                    audioSource.PlayOneShot(sound);
-                    isMovingToRight = false;
-                }
-            }
-            else
+            if (currentAngle > Settings.movingAngle)
             {
-                MovePoint.transform.RotateAround(Settings.cameraPosition, Vector3.up, -Settings.movingSpeed() * Time.deltaTime);
-                currentAngle -= Settings.movingSpeed() * Time.deltaTime;
-
-                if (currentAngle < -Settings.movingAngle)
-                {
-                    audioSource.PlayOneShot(sound);
-                    isMovingToRight = true;
-                }
+                audioSource.PlayOneShot(sound);
+                isMovingToRight = false;
             }
         }
         else
+        {
+            MovingPoint.transform.RotateAround(Settings.cameraPosition, Vector3.up, -Settings.movingSpeed() * Time.deltaTime);
+            currentAngle -= Settings.movingSpeed() * Time.deltaTime;
+
+            if (currentAngle < -Settings.movingAngle)
+            {
+                audioSource.PlayOneShot(sound);
+                isMovingToRight = true;
+            }
+        }
+    }
+
+    private void DoExperiment()
+    {
+        if (step == 0) // 現在のトライアル数を表示中
+        {
+            if (RespTrigger.GetState(HandType))
+            {
+                ChangeGuideText(null, false);
+                FixationPoint.SetActive(true);
+                step++;
+            }
+        }
+        else if (step == 1) // 一定回数反復中
         {
             localTime += Time.deltaTime;
             if (localTime > Settings.movingOneWayTime)
             {
                 audioSource.PlayOneShot(sound);
                 localTime = 0;
+                if (++shotCount == Settings.shotCount)
+                {
+                    step++;
+                }
+            }
+        }
+        else if (step == 2) // 刺激提示直前(反復中心で刺激を提示するための調整を行う)
+        {
+            localTime += Time.deltaTime;
+            if (localTime > (Settings.movingOneWayTime - Settings.stimulusPresentationTime) / 2)
+            {
+                stimulusPresentationStartTime = time;
+                localTime = 0;
+                TopBelt.SetActive(true);
+                BottomBelt.SetActive(true);
+                step++;
+            }
+        }
+        else if (step == 3) // 刺激提示中
+        {
+            if (topBeltMoveToRight[currentTrial - 1])
+            {
+                TopBelt.transform.Translate(Tan(beltSpeedToRight[currentTrial - 1]) * Time.deltaTime, 0, 0);
+                BottomBelt.transform.Translate(-Tan(beltSpeedToLeft[currentTrial - 1]) * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                TopBelt.transform.Translate(-Tan(beltSpeedToLeft[currentTrial - 1]) * Time.deltaTime, 0, 0);
+                BottomBelt.transform.Translate(Tan(beltSpeedToRight[currentTrial - 1]) * Time.deltaTime, 0, 0);
+            }
+            localTime += Time.deltaTime;
+            if (localTime > Settings.stimulusPresentationTime)
+            {
+                TopBelt.SetActive(false);
+                BottomBelt.SetActive(false);
+                FixationPoint.SetActive(false);
+                step++;
+            }
+        }
+        else if (step == 4) // 応答待ち
+        {
+            if (RespUp.GetState(HandType))
+            {
+                WriteExData(true);
+                step++;
+            }
+
+            if (RespDown.GetState(HandType))
+            {
+                WriteExData(false);
+                step++;
+            }
+        }
+        else if (step == 5)
+        {
+            if (currentTrial == Settings.nTrials())
+            {
+                step++;
+            }
+            else
+            {
+                // 初期化処理
+                TopBelt.transform.localPosition = Settings.topBeltPosition;
+                BottomBelt.transform.localPosition = Settings.bottomBeltPosition;
+                localTime = 0;
+                shotCount = 0;
+                currentTrial++;
+                ChangeGuideText(trialText(currentTrial), true);
+                step = 0;
             }
         }
     }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,12 +3,25 @@
 public class Settings
 {
     public static Vector3 cameraPosition = new Vector3(0, 0, 0);
-    //public static Vector3 fixationPointPosition = new Vector3(0, 0, 50); // 大きさは直径約1.14deg
+    public static Vector3 topBeltPosition = new Vector3(0, 0.1f, 1);
+    public static Vector3 bottomBeltPosition = new Vector3(0, -0.1f, 1);
     public static float movingAngle = 50; // 片側の動く角度[deg]
     public static float movingOneWayTime = 1.0f; // 片道の時間[sec]
+    public static float stimulusPresentationTime = 0.5f; // 刺激提示時間[sec]
 
     public static float movingSpeed()
     {
         return movingAngle * 2 / movingOneWayTime; // [deg/s]
     }
+
+    public static int repeat = 2;
+    public static double[] ratio = new double[] {-0.4, -0.32, -0.24, -0.16, -0.08, 0, 0.08, 0.16, 0.24, 0.32, 0.4};
+
+    public static int nTrials()
+    {
+        return ratio.Length * repeat;
+    }
+
+    public static int shotCount = 6; // この回数分だけ反復した後に刺激が提示される
+    public static float beltBaseSpeed = 12.0f; // [deg/s]
 }
