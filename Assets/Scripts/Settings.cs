92a94a06bdf0e5f960051761eff3e3fde58a10df
Fukky21 <r.fukky21@gmail.com>
2020-12-11 18:36:44 +0900

    [Fix] 細かい修正

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -7,7 +7,7 @@ public class Settings
     public static float beltBaseSpeed = 12.0f; // [deg/s]
     public static float movingOneWayTime = 1.0f; // 片道の時間[sec]
     public static float stimulusPresentationTime = 0.5f; // 刺激提示時間[sec]
-    public static double initialRatio = 2.5;
+    public static double initialRatio = 2.5; // Default: 2.5
     public static double ratioStep = 0.1;
     public static int consecutiveCorrentAnswers = 2; // 次のratioへ移動するために必要な連続正解数
     public static int trials = 40;
